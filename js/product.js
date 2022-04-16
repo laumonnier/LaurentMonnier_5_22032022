c45cef29cc21806cb6836866fa78b5fcfaf70876
laumonnier <laurent.p.monnier86@gmail.com>
2022-04-16 14:23:56 +0200

    - HTTP request to request all desired products from the API - Additions of the various functions allowing the integration of the various products from the API into the web page. - Implementation of the various products on the home page

---
+++ js/product.js
@@ -0,0 +1,47 @@
+const api_url =  'http://localhost:3000/api/products';
+
+function getProducts(){
+    fetch(api_url)
+    .then(function(res){
+        if (res.ok){
+            return res.json();
+        }
+    })
+    .then(function(data){
+        const products = data;
+        console.log(products);
+        addTagImgInDiv();
+
+
+        function addTagImgInDiv(){
+            var newElmtImg = document.createElement("img");
+            Object.assign(newElmtImg,{
+                src : "http://localhost:3000/images/kanap01.jpeg",
+                alt : "Photographie du"&products[i].name
+            })
+            document.getElementsByClassName('item__img').appendChild(newElmtImg);
+        }
+
+        function addProductTitle(){
+            var id = products[i]._id;
+            if (id = products[i]._id){
+                innerText = products[i].name;
+            }else{
+                alert ("The id don't existing !")
+            }
+        }
+        var imageUrl = products[0].imageUrl;
+        var url = new URL(imageUrl);
+        var name = url.searchParams.get(url);
+        console.log(imageUrl);
+        console.log(name);
+    })
+    .catch(function(err){
+        err = console.log("There is an error in the request");
+    })
+} 
+
+getProducts();
+
+let s = "http://localhost:3000/api/products";
+let a = new URL("/",s);
