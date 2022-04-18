55be7ec2f6a1533d955cd8a6c61a48d46de61eb3
laumonnier <laurent.p.monnier86@gmail.com>
2022-04-18 19:55:30 +0200

    Setting a Test function to determine if the id is correct

--- front/js/product.js
+++ front/js/product.js
@@ -9,16 +9,38 @@ function getProducts(){
     })
     .then(function(data){
         const products = data;
+        const pagelocation = window.location.href;
+        const newUrlImg = new URL(pagelocation);
+        const search_Id = new URLSearchParams(newUrlImg.search);
+        testIdExist();
+        const idProduct = newUrlImg.searchParams.get("id");
+        
         console.log(products);
         console.log(location.href);
-        // addTagImgToDiv();
-        const pagelocation = window.location.href;
-        var newUrlImg = new URL(pagelocation);
-        var idProduct = newUrlImg.searchParams.get("id");
-        console.log(idProduct);
+        addTagImgToDiv();
+        
+        
+        function testIdExist(){
+            if(search_Id.has('id')){
+                const idProduct = newUrlImg.searchParams.get("id");
+                console.log(idProduct);
+                try {
+                    for(var i in products){
+                        idProduct === products[i]._id;
+                    }    
+                }
+                catch{
+                    console.log("This id isn't existing in api !!!")
+                }
+            }else{
+                alert("There is no id!")
+            }   
+        }
+
 
         function addTagImgToDiv(){
             const newElmtImg = document.createElement("img");
+            
             Object.assign(newElmtImg,{
                 src : "http://localhost:3000/images/kanap01.jpeg",
                 alt : `Photographie du ${products[i].name}`
