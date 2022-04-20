ee6d4133b0c32507deb75b961031d63035dd6252
laumonnier <laurent.p.monnier86@gmail.com>
2022-04-20 15:36:11 +0200

    Layout of the product.html page Use the id of the chosen product to focus on the correct attributes of the object. Then add the function addTagImgToDiv to add the photo of the desired article

--- front/js/product.js
+++ front/js/product.js
@@ -1,73 +1,103 @@
-const api_url =  'http://localhost:3000/api/products';
+/**The "getIdToLocation()" function to return the value of the "id" for the chosen product on the "index.html" page */
+function getIdToLocation(){
+    pageLocation = window.location.href;
+    const newUrl = new URL(pageLocation);
+    const idProduct = newUrl.searchParams.get("id");
+    return idProduct;
+}
+const newId = getIdToLocation();
+console.log(newId);
 
-function getProducts(){
-    fetch(api_url)
+/**The "productGet()" function allows you to get the API product on the local server port 3000!
+Dynamically retransmit them on the product.html. */
+const api_url = 'http://localhost:3000/api/products/';
+function productGet(){
+    fetch(api_url + newId)
     .then(function(res){
-        if (res.ok){
+        if(res.ok){
             return res.json();
         }
     })
     .then(function(data){
-        const products = data;
-        const pagelocation = window.location.href;
-        const newUrlImg = new URL(pagelocation);
-        const search_Id = new URLSearchParams(newUrlImg.search);
-        testIdExist();
-        const idProduct = newUrlImg.searchParams.get("id");
-        
-        console.log(products);
-        console.log(location.href);
+        const product = data;
+        console.log(product);
+
         addTagImgToDiv();
-        
-        
-        function testIdExist(){
-            if(search_Id.has('id')){
-                const idProduct = newUrlImg.searchParams.get("id");
-                console.log(idProduct);
-                try {
-                    for(var i in products){
-                        idProduct === products[i]._id;
-                    }    
-                }
-                catch{
-                    console.log("This id isn't existing in api !!!")
-                }
-            }else{
-                alert("There is no id!")
-            }   
-        }
 
-
+         /**Function "addTagImgToDiv()" to create a tag "img", with its attributes, child of the tag "div" to classe "class = item__img" for one product of the API. Especially to the getElementsByClassName() method. */
         function addTagImgToDiv(){
-            const newElmtImg = document.createElement("img");
-            
-            Object.assign(newElmtImg,{
-                src : "http://localhost:3000/images/kanap01.jpeg",
-                alt : `Photographie du ${products[i].name}`
+            const newImgElmt = document.createElement("img");
+            Object.assign(newImgElmt,{
+                src : product.imageUrl,
+                alt : product.altTxt
             })
-            document.getElementsByClassName('item__img').appendChild(newElmtImg);
+            var listElmt = document.getElementsByClassName("item__img");
+            listElmt[0].appendChild(newImgElmt);    
         }
 
-        function addProductTitle(){
-            var id = products[i]._id;
-            if (id = products[i]._id){
-                innerText = products[i].name;
-            }else{
-                alert ("The id don't existing !")
-            }
-        }
-        var imageUrl = `&{products[0].imageUrl}`;
-        var url = new URL(imageUrl);
-        var name = url.searchParams.get(url);
-        console.log(imageUrl);
-        console.log(name);
     })
     .catch(function(err){
         err = console.log("There is an error in the request");
     })
-} 
+}
+productGet();
 
-getProducts();
 
-let s = "http://localhost:3000/api/products";
-let a = new URL("/",s);
+
+// const api_url =  'http://localhost:3000/api/products';
+
+// function getProducts(){
+//     fetch(api_url)
+//     .then(function(res){
+//         if (res.ok){
+//             return res.json();
+//         }
+//     })
+//     .then(function(data){
+//         const products = data;
+//         const pagelocation = window.location.href;
+//         const newUrlImg = new URL(pagelocation);
+//         const search_Id = new URLSearchParams(newUrlImg.search);
+//         testIdExist();
+//         const idProduct = newUrlImg.searchParams.get("id");
+        
+//         console.log(products);
+//         console.log(location.href);
+//         addTagImgToDiv();
+        
+        
+//         function testIdExist(){
+//             if(search_Id.has('id')){
+//                 const idProduct = newUrlImg.searchParams.get("id");
+//                 console.log(idProduct);
+//                 try {
+//                     for(var i in products){
+//                         idProduct === products[i]._id;
+//                     }    
+//                 }
+//                 catch{
+//                     console.log("This id isn't existing in api !!!")
+//                 }
+//             }else{
+//                 alert("There is no id!")
+//             }   
+//         }
+
+//         function addProductTitle(){
+//             var id = products[i]._id;
+//             if (id = products[i]._id){
+//                 innerText = products[i].name;
+//             }else{
+//                 alert ("The id don't existing !")
+//             }
+//         }
+//         var imageUrl = `&{products[0].imageUrl}`;
+//         var url = new URL(imageUrl);
+//         var name = url.searchParams.get(url);
+//         console.log(imageUrl);
+//         console.log(name);
+//     })
+//     .catch(function(err){
+//         err = console.log("There is an error in the request");
+//     })
+// } 
