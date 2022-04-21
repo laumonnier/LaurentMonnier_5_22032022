e59572d46f9f834afc326fb444dbf66e3fc9fafe
laumonnier <laurent.p.monnier86@gmail.com>
2022-04-21 16:49:38 +0200

    Addition of several functions allowing the dynamic addition of several elements in the section element of the cart.html. The functions allow us to go to the div element having the cart__item__content__settings class.

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,7 +5,7 @@
 const parentNodeSection =  document.getElementById("cart__items");
 
 /**Allows you to add an "article" element to the Section element as a child, with its attributes. */
-function addChildCartSection(){
+function addChildSection(){
     const newArticleElmt = document.createElement("article");
     Object.assign(newArticleElmt,{
         class : "cart__item",
@@ -16,7 +16,7 @@ function addChildCartSection(){
 }
 
 /**Allows you to add a "div" element to the "article" element as a child, with its attributes. */
-function addChildCartArticle(){
+function addChildArticle(){
     const newDivElmtToArticle = document.createElement("div");
     Object.assign(newDivElmtToArticle,{
         class : "cart__item__img"
@@ -25,7 +25,7 @@ function addChildCartArticle(){
 } 
 
 /**Allows you to add an "img" element to the "div" element as a child, with its attributes. */
-function addChildCartDiv(){
+function addChildDiv(){
     const newImgElmtToDiv = document.createElement("img");
     Object.assign(newImgElmtToDiv,{
         src : product.imageUrl,
@@ -39,4 +39,78 @@ function addChildCartDiv(){
     }
 }
 
+/**Allows you to add a "div" element to the "article" element as a child. */
+function addChild_2Article(){
+    const newDiv_2ElmtToArticle = document.createElement("div");
+    Object.assign(newFirstDivElmtToDiv,{
+        class : "cart__item__content"
+    })
+    document.getElementById("{product._id}").appendChild(newDiv_2ElmtToArticle);    
+}
+
+/**Allows you to add a first "div" element to the "div" element as a child. */
+function addFirstChildToDiv(){
+    const newFirstDivElmtToDiv = document.createElement("div");
+    Object.assign(newFirstDivElmtToDiv,{
+        class : "cart__item__content__description"
+    })
+    var listElmt = document.getElementsByClassName("cart_item_content");
+    if(listElmt.lenght != 0){
+        listElmt[0].appendChild(newFirstDivElmtToDiv);
+    }else{
+        console.log("There is no class : 'cart_item_content' !");
+    }
+}
+
+//The function retrieves a list of items obtained by their class name
+/**
+ * Recovering data
+ * @param { String } className
+ * @param { Integer } nbr
+ * @param { Object } newElmt
+ */
+function listElmt(className, nbr, newElmt){
+    var listElmt = document.getElementsByClassName(className)
+    if(listElmt.lenght != 0){
+        listElmt[nbr].appendChild(newElmt);
+    }else{
+        console.log("There is no class : " + className + " !");
+    }
+}
+
+/**Add a "title" element to the second  "div" node as a child in the "section". */
+function addChildTitleFirstDiv(){
+    const newTitleElmtToDiv = document.createElement("h2");
+    Object.assign(newTitleElmtToDiv,{
+        InnerText : product.name
+    })
+    listElmt("cart__item__content__description", 0, newTitleElmtToDiv);
+}
+
+/**Allows to add, in the "section", a first element "p" to the second node  "div" as a child. */
+function addFirstChildPgrphFirstDiv(){
+    const newPrgphElmtToDiv = document.createElement("p");
+    Object.assign(newPrgphElmtToDiv,{
+        innerText : product.colors[color]
+    })
+    listElmt("cart__item__content__description", 0, newPrgphElmtToDiv);
+}
+
+/**Allows to add, in the "section", a second element "p" to the second node  "div" as a child. */
+function addSecdChildPgrphFirstDiv(){
+    const newPrgphElmtToDiv = document.createElement("p");
+    Object.assign(newPrgphElmtToDiv,{
+        innerText : product.price
+    })
+    listElmt("cart__item__content__description", 0, newPrgphElmtToDiv);
+}
+
+/**Allows you to add a second "div" element to the "div" element as a child. */
+function addSecdChildToDiv(){
+    const newSecdDivElmtToDiv = document.createElement("div");
+    Object.assign(newSecdDivElmtToDiv,{
+        class : "cart__item__content__settings"
+    })
+    listElmt("cart__item__content", 0, newSecdDivElmtToDiv);
+}
 
