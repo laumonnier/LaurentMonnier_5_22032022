33b4cd69fedf9ba49d5aee48ee9c877be6ec9b99
laumonnier <laurent.p.monnier86@gmail.com>
2022-04-21 18:20:18 +0200

    Implementation of all the elements in the article part for a dynamic change afterwards

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,9 +3,26 @@
 
 
 const parentNodeSection =  document.getElementById("cart__items");
+const newDivElmt = document.createElement("div");
+const newPrgphElmt = document.createElement("p");
+//The function retrieves a list of items obtained by their class name
+/**
+ * Recovering data
+ * @param { String } className
+ * @param { Integer } nbr
+ * @param { Object } newElmt
+ */
+ function listElmt(className, nbr, newElmt){
+    var listElmt = document.getElementsByClassName(className)
+    if(listElmt.lenght != 0){
+        listElmt[nbr].appendChild(newElmt);
+    }else{
+        console.log("There is no class : " + className + " !");
+    }
+}
 
-/**Allows you to add an "article" element to the Section element as a child, with its attributes. */
-function addChildSection(){
+/**Addition of an element "article" corresponding to an article, and it is in this part that our articles will be added or deleted or modified */
+function addArticleToSection(){
     const newArticleElmt = document.createElement("article");
     Object.assign(newArticleElmt,{
         class : "cart__item",
@@ -15,102 +32,116 @@ function addChildSection(){
     parentNodeSection.appendChild(newArticleElmt);
 }
 
-/**Allows you to add a "div" element to the "article" element as a child, with its attributes. */
-function addChildArticle(){
-    const newDivElmtToArticle = document.createElement("div");
-    Object.assign(newDivElmtToArticle,{
+/**Addition of a "div" element corresponding to the "image" part */
+function addImageDiv(){
+    Object.assign(newDivElmt,{
         class : "cart__item__img"
     })
-    document.getElementsById("{product._id}").appendChild(newDivElmtToArticle);
+    document.getElementsById("{product._id}").appendChild(newDivElmt);
 } 
 
-/**Allows you to add an "img" element to the "div" element as a child, with its attributes. */
-function addChildDiv(){
-    const newImgElmtToDiv = document.createElement("img");
-    Object.assign(newImgElmtToDiv,{
+/**Added an element "img" to insert the image of the article */
+function addProductImage(){
+    const newImgElmt = document.createElement("img");
+    Object.assign(newImgElmt,{
         src : product.imageUrl,
         alt : product.altTxt
     })
-    const listElmt = document.getElementsByClassName("cart__item__img");
-    if(listElmt.length != 0){
-        listElmt[0].appendChild(newImgElmtToDiv);
-    }else{
-        console.log("There is no class : 'cart__item__img' !")
-    }
+    listElmt("cart__item__img", 0, newImgElmt);
 }
 
-/**Allows you to add a "div" element to the "article" element as a child. */
-function addChild_2Article(){
-    const newDiv_2ElmtToArticle = document.createElement("div");
-    Object.assign(newFirstDivElmtToDiv,{
+/**Addition of a "div" element to indicate the "content" part */
+function addContentDiv(){
+    Object.assign(newDivElmt,{
         class : "cart__item__content"
     })
-    document.getElementById("{product._id}").appendChild(newDiv_2ElmtToArticle);    
+    document.getElementById("{product._id}").appendChild(newDivElmt);    
 }
 
-/**Allows you to add a first "div" element to the "div" element as a child. */
-function addFirstChildToDiv(){
-    const newFirstDivElmtToDiv = document.createElement("div");
-    Object.assign(newFirstDivElmtToDiv,{
+/**Addition of a "div" element to indicate the "description" part */
+function addDescriptionDiv(){
+    Object.assign(newDivElmt,{
         class : "cart__item__content__description"
     })
-    var listElmt = document.getElementsByClassName("cart_item_content");
-    if(listElmt.lenght != 0){
-        listElmt[0].appendChild(newFirstDivElmtToDiv);
-    }else{
-        console.log("There is no class : 'cart_item_content' !");
-    }
+    listElmt("cart_item_content", 0, newDivElmt);
 }
 
-//The function retrieves a list of items obtained by their class name
-/**
- * Recovering data
- * @param { String } className
- * @param { Integer } nbr
- * @param { Object } newElmt
- */
-function listElmt(className, nbr, newElmt){
-    var listElmt = document.getElementsByClassName(className)
-    if(listElmt.lenght != 0){
-        listElmt[nbr].appendChild(newElmt);
-    }else{
-        console.log("There is no class : " + className + " !");
-    }
-}
-
-/**Add a "title" element to the second  "div" node as a child in the "section". */
-function addChildTitleFirstDiv(){
-    const newTitleElmtToDiv = document.createElement("h2");
-    Object.assign(newTitleElmtToDiv,{
+/**Addition of an element "h2" to indicate the title of the article */
+function addProductNameTitle(){
+    const newTitleElmt = document.createElement("h2");
+    Object.assign(newTitleElmt,{
         InnerText : product.name
     })
-    listElmt("cart__item__content__description", 0, newTitleElmtToDiv);
+    listElmt("cart__item__content__description", 0, newTitleElmt);
 }
 
-/**Allows to add, in the "section", a first element "p" to the second node  "div" as a child. */
-function addFirstChildPgrphFirstDiv(){
-    const newPrgphElmtToDiv = document.createElement("p");
-    Object.assign(newPrgphElmtToDiv,{
+/**Add a "p" element to indicate the color of the article */
+function addColorPgrph(){
+    Object.assign(newPrgphElmt,{
         innerText : product.colors[color]
     })
-    listElmt("cart__item__content__description", 0, newPrgphElmtToDiv);
+    listElmt("cart__item__content__description", 0, newPrgphElmt);
 }
 
-/**Allows to add, in the "section", a second element "p" to the second node  "div" as a child. */
-function addSecdChildPgrphFirstDiv(){
-    const newPrgphElmtToDiv = document.createElement("p");
-    Object.assign(newPrgphElmtToDiv,{
+/**Add a "p" element to indicate the price of the item */
+function addPricePgrph(){
+    Object.assign(newPrgphElmt,{
         innerText : product.price
     })
-    listElmt("cart__item__content__description", 0, newPrgphElmtToDiv);
+    listElmt("cart__item__content__description", 0, newPrgphElmt);
 }
 
-/**Allows you to add a second "div" element to the "div" element as a child. */
-function addSecdChildToDiv(){
-    const newSecdDivElmtToDiv = document.createElement("div");
-    Object.assign(newSecdDivElmtToDiv,{
+/**Addition of a "div" element to indicate the part corresponding to "settings" */
+function addSettingsDiv(){
+    Object.assign(newDivElmt,{
         class : "cart__item__content__settings"
     })
-    listElmt("cart__item__content", 0, newSecdDivElmtToDiv);
+    listElmt("cart__item__content", 0, newDivElmt);
 }
 
+/**Addition of a "div" element to indicate things in relation to the quantity */
+function addSettingsQtyDiv(){
+    Object.assign(newDivElmt,{
+        class : "cart__item__content__settings__quantity"
+    })
+    listElmt("cart__item__content__settings", 0, newDivElmt);
+}
+
+/**Addition of a "p" element concerning the quantity */
+function addQtityPargrph(){
+    Object.assign(newPrgphElmt,{
+        innerText : "Qté : "
+    })
+    listElmt("cart__item__content__settings__quantity", 0, newPrgphElmt);
+}
+
+/**Add an "input" element to display the quantity */
+function addQtityInput(){
+    const newInputElmt = document.createElement("input");
+    Object.assign(newInputElmt,{
+        type : "number",
+        class : "itemQuantity",
+        name : "itemQuantity",
+        min : "1",
+        max : "100",
+        value : "42",
+    })
+    listElmt("cart__item__content__settings__quantity", 0, newInputElmt);
+}
+
+/**Addition of a "div" element to include "settings Delete" */
+function addSettingsDeleteDiv(){
+    Object.assign(newDivElmt,{
+        class : "cart__item__content__settings__delete"
+    })
+    listElmt("cart__item__content__settings", 0, newDivElmt);
+}
+
+/**Add a "p" element to display the term "supprimer" */
+function addDeleteItemPargrph(){
+    Object.assign(newPrgphElmt,{
+        class : "deleteItem",
+        innerText : "Supprimer"
+    })
+    listElmt("cart__item__content__settings__delete", 0, newPrgphElmt);
+}
