0867c8fc362b77601567a424361ded23641f8f0c
laumonnier <laurent.p.monnier86@gmail.com>
2022-05-03 12:04:21 +0200

    Completion of step 8 and positive tests for all functions

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,10 +1,10 @@
 const api_url = 'http://localhost:3000/api/products/';
 var itemsLocalStorage = JSON.parse(localStorage.getItem("item"));
-console.log(itemsLocalStorage);
+console.log(itemsLocalStorage); //Test positif
 
 for(var i = 0 ; i < itemsLocalStorage.length ; i++){
 
-    async function getProducts(i) {
+    async function getProduct(i) {
         fetch(api_url + itemsLocalStorage[i].id)
         .then(function(res){
             if(res.ok) {
@@ -14,9 +14,9 @@ for(var i = 0 ; i < itemsLocalStorage.length ; i++){
         
         .then(function(data){
             const product = data;
-            console.log(itemsLocalStorage[i]);
-            console.log(itemsLocalStorage[i].color);
-            console.log(itemsLocalStorage[i].quantity);
+            console.log(product); //Test positif
+            console.log(itemsLocalStorage[i].color); //Test positif
+            console.log(itemsLocalStorage[i].quantity); //Test positif
             const parentNodeSection =  document.getElementById("cart__items");
             const newDivElmt = document.createElement("div");
             const newPrgphElmt = document.createElement("p");
@@ -37,37 +37,37 @@ for(var i = 0 ; i < itemsLocalStorage.length ; i++){
             }
 
             addArticleToSection(i);
-            // addImageDiv(i);
-            // addProductImage();
-            // addContentDiv(i);
-            // addDescriptionDiv();
-            // addProductNameTitle();
-            // addColorPgrph(i);
-            // addPricePgrph();
-            // addSettingsDiv();
-            // addSettingsQtyDiv();
-            // addQtityPargrph();
-            // addQtityInput(i);
-            // addSettingsDeleteDiv();
-            // addDeleteItemPargrph();
+            addImageDiv(i);
+            addProductImage(i);
+            addContentDiv(i);
+            addDescriptionDiv(i);
+            addProductNameTitle(i);
+            addColorPgrph(i);
+            addPricePgrph(i);
+            addSettingsDiv(i);
+            addSettingsQtyDiv(i);
+            addQtityPargrph(i);
+            addQtityInput(i);
+            addSettingsDeleteDiv(i);
+            addDeleteItemPargrph(i);
 
             /**Addition of an element "article" corresponding to an article, and it is in this part that our articles will be added or deleted or modified */
             function addArticleToSection(i){
                 const newArticleElmt = document.createElement("article");
-                Object.assign(newArticleElmt,{
-                    class : "cart__item"
-                })
+                newArticleElmt.setAttribute("class", "cart__item")
                 newArticleElmt.setAttribute("data-id", `product-${i}`);
                 newArticleElmt.setAttribute("data-color", itemsLocalStorage[i].color);
                 parentNodeSection.appendChild(newArticleElmt);
+                console.log(newArticleElmt); //Test positif
             }
 
             /**Addition of a "div" element corresponding to the "image" part */
             function addImageDiv(i){
-                Object.assign(newDivElmt,{
-                    class : "cart__item__img"
-                })
-                document.getElementById(`product-${i}`).appendChild(newDivElmt);
+                const newDivElmt = document.createElement("div");
+                newDivElmt.setAttribute("class", "cart__item__img");
+                listElmt("cart__item", i, newDivElmt);
+                // document.getElementById(`product-${i}`).appendChild(newDivElmt);
+                console.log(newDivElmt); //Test positif
             } 
 
             /**Added an element "img" to insert the image of the article */
@@ -77,72 +77,74 @@ for(var i = 0 ; i < itemsLocalStorage.length ; i++){
                     src : `${product.imageUrl}`,
                     alt : `${product.altTxt}`
                 })
-                listElmt("cart__item__img", 0, newImgElmt);
+                listElmt("cart__item__img", i, newImgElmt);
+                console.log(newImgElmt); //Test positif
             }
 
             /**Addition of a "div" element to indicate the "content" part */
             function addContentDiv(i){
-                Object.assign(newDivElmt,{
-                    class : "cart__item__content"
-                })
-                document.getElementById(`product-${i}`).appendChild(newDivElmt);    
+                const newDivElmt = document.createElement("div");
+                newDivElmt.setAttribute("class", "cart__item__content");
+                listElmt("cart__item", i, newDivElmt);
+                // document.getElementById(`product-${i}`).appendChild(newDivElmt);
+                console.log(newDivElmt); //Test positif    
             }
 
             /**Addition of a "div" element to indicate the "description" part */
-            function addDescriptionDiv(){
-                Object.assign(newDivElmt,{
-                    class : "cart__item__content__description"
-                })
-                listElmt("cart_item_content", 0, newDivElmt);
+            function addDescriptionDiv(i){
+                const newDivElmt = document.createElement("div");
+                newDivElmt.setAttribute("class", "cart__item__content__description");
+                listElmt("cart__item__content", i, newDivElmt);
+                console.log(newDivElmt); //Test positif
             }
 
             /**Addition of an element "h2" to indicate the title of the article */
-            function addProductNameTitle(){
+            function addProductNameTitle(i){
                 const newTitleElmt = document.createElement("h2");
-                Object.assign(newTitleElmt,{
-                    InnerText : `${product.name}`
-                })
-                listElmt("cart__item__content__description", 0, newTitleElmt);
+                newTitleElmt.innerText = `${product.name}`;
+                listElmt("cart__item__content__description", i, newTitleElmt);
+                console.log(newTitleElmt); //Test positif
             }
 
             /**Add a "p" element to indicate the color of the article */
             function addColorPgrph(i){
-                Object.assign(newPrgphElmt,{
-                    innerText : `${itemsLocalStorage[i].color}`
-                })
-                listElmt("cart__item__content__description", 0, newPrgphElmt);
+                const newPrgphElmt = document.createElement("p");
+                newPrgphElmt.innerText = `${itemsLocalStorage[i].color}`;
+                listElmt("cart__item__content__description", i, newPrgphElmt);
+                console.log(newPrgphElmt); //Test positif
             }
 
             /**Add a "p" element to indicate the price of the item */
-            function addPricePgrph(){
-                Object.assign(newPrgphElmt,{
-                    innerText : product.price
-                })
-                listElmt("cart__item__content__description", 0, newPrgphElmt);
+            function addPricePgrph(i){
+                const newPrgphElmt = document.createElement("p");
+                newPrgphElmt.innerText = product.price + " €";
+                listElmt("cart__item__content__description", i, newPrgphElmt);
+                console.log(newPrgphElmt); //Test positif
             }
 
             /**Addition of a "div" element to indicate the part corresponding to "settings" */
-            function addSettingsDiv(){
-                Object.assign(newDivElmt,{
-                    class : "cart__item__content__settings"
-                })
-                listElmt("cart__item__content", 0, newDivElmt);
+            nbr = 1+2*i;
+            function addSettingsDiv(nbr){
+                const newDivElmt = document.createElement("div");
+                newDivElmt.setAttribute("class", "cart__item__content__settings");
+                listElmt("cart__item__content", nbr, newDivElmt);
+                console.log(newDivElmt); //Test positif
             }
 
             /**Addition of a "div" element to indicate things in relation to the quantity */
-            function addSettingsQtyDiv(){
-                Object.assign(newDivElmt,{
-                    class : "cart__item__content__settings__quantity"
-                })
-                listElmt("cart__item__content__settings", 0, newDivElmt);
+            function addSettingsQtyDiv(i){
+                const newDivElmt = document.createElement("div");
+                newDivElmt.setAttribute("class", "cart__item__content__settings__quantity");
+                listElmt("cart__item__content__settings", i, newDivElmt);
+                console.log(newDivElmt); //Test positif
             }
 
             /**Addition of a "p" element concerning the quantity */
-            function addQtityPargrph(){
-                Object.assign(newPrgphElmt,{
-                    innerText : "Qté : "
-                })
-                listElmt("cart__item__content__settings__quantity", 0, newPrgphElmt);
+            function addQtityPargrph(i){
+                const newPrgphElmt = document.createElement("p");
+                newPrgphElmt.innerText = "Qté : ";
+                listElmt("cart__item__content__settings__quantity", i, newPrgphElmt);
+                console.log(newPrgphElmt); //Test positif
             }
 
             /**Add an "input" element to display the quantity */
@@ -156,29 +158,30 @@ for(var i = 0 ; i < itemsLocalStorage.length ; i++){
                     max : "100",
                     value : `${itemsLocalStorage[i].quantity}`,
                 })
-                listElmt("cart__item__content__settings__quantity", 0, newInputElmt);
+                listElmt("cart__item__content__settings__quantity", i, newInputElmt);
+                console.log(newInputElmt); //Test positif
             }
 
             /**Addition of a "div" element to include "settings Delete" */
-            function addSettingsDeleteDiv(){
-                Object.assign(newDivElmt,{
-                    class : "cart__item__content__settings__delete"
-                })
-                listElmt("cart__item__content__settings", 0, newDivElmt);
+            function addSettingsDeleteDiv(nbr){
+                const newDivElmt = document.createElement("div");
+                newDivElmt.setAttribute("class", "cart__item__content__settings__delete");
+                listElmt("cart__item__content__settings", nbr, newDivElmt);
+                console.log(newDivElmt); //Test positif
             }
 
             /**Add a "p" element to display the term "supprimer" */
-            function addDeleteItemPargrph(){
-                Object.assign(newPrgphElmt,{
-                    class : "deleteItem",
-                    innerText : "Supprimer"
-                })
-                listElmt("cart__item__content__settings__delete", 0, newPrgphElmt);
+            function addDeleteItemPargrph(i){
+                const newPrgphElmt = document.createElement("p");
+                newPrgphElmt.setAttribute("class", "deleteItem");
+                newPrgphElmt.innerText = "Supprimer";
+                listElmt("cart__item__content__settings__delete", i, newPrgphElmt);
+                console.log(newPrgphElmt); //Test positif
             }
         })
         .catch(function(err){
             err = console.log("There is an error in the request !");
         })    
     }
-    getProducts(i);
+    getProduct(i);
 }
