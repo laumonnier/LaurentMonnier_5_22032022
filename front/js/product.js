f376c2d2b5cc9f162d809646401dbaf8db48661e
laumonnier <laurent.p.monnier86@gmail.com>
2022-06-15 15:20:56 +0200

    Removal of unnecessary coding

--- front/js/product.js
+++ front/js/product.js
@@ -96,49 +96,49 @@ function productGet(){
                     quantity : productQty, 
                     color : productColor
                 }
-                if(productColor === ""){
-                    alert('Il faut choisir une couleur SVP !!!');
-                    document.location.reload();
-                    addArticleToCart();
-                }
                 if(productQty === 0){
                     alert('Il faut choisir une quantité différente de "0" SVP !!!');
                     document.location.reload();
                     addArticleToCart();
+                }
+                if(productColor === ""){
+                    alert('Il faut choisir une couleur SVP !!!');
+                    document.location.reload();
+                    addArticleToCart();
                 }else{
-                var itemsLocalStorage = JSON.parse(localStorage.getItem("item"));
-                // console.log(itemsLocalStorage);
-                if(itemsLocalStorage != null){
-                    var itemOk = 0;
-                    for (var i = 0; i < itemsLocalStorage.length; i++){
-                        // console.log(itemsLocalStorage[i].id);
-                        // console.log(itemsLocalStorage[i].quantity);
-                        // console.log(itemsLocalStorage[i].color);
-                        if ((item.id === itemsLocalStorage[i].id) && (item.color === itemsLocalStorage[i].color)){
-                            // console.log("Coucou"); //Vérifier
-                            itemOk++;
-                            itemsLocalStorage[i].quantity = parseInt(itemsLocalStorage[i].quantity);
-                            itemsLocalStorage[i].quantity += item.quantity; // Vérifier cette ligne
+                    var itemsLocalStorage = JSON.parse(localStorage.getItem("item"));
+                    // console.log(itemsLocalStorage);
+                    if(itemsLocalStorage != null){
+                        var itemOk = 0;
+                        for (var i = 0; i < itemsLocalStorage.length; i++){
+                            // console.log(itemsLocalStorage[i].id);
                             // console.log(itemsLocalStorage[i].quantity);
-                            localStorage.setItem('item', JSON.stringify(itemsLocalStorage));
-                            // console.log(itemsLocalStorage.length);
-                        }
-                    }    
-                    if(itemOk === 0){
-                            itemsLocalStorage.push(item);
-                            // console.log("Hallo"); // Vérifier
-                            localStorage.setItem('item', JSON.stringify(itemsLocalStorage));
-                            // console.log(itemsLocalStorage.length);
-                    }               
-                }else{
-                    itemsLocalStorage = [];
-                    itemsLocalStorage.push(item);
-                    // console.log("Laurent");
-                    localStorage.setItem('item', JSON.stringify(itemsLocalStorage));
-                    // console.log(itemsLocalStorage.length);
-                }
-                }
-            })
+                            // console.log(itemsLocalStorage[i].color);
+                            if ((item.id === itemsLocalStorage[i].id) && (item.color === itemsLocalStorage[i].color)){
+                                // console.log("Coucou"); //Vérifier
+                                itemOk++;
+                                itemsLocalStorage[i].quantity = parseInt(itemsLocalStorage[i].quantity);
+                                itemsLocalStorage[i].quantity += item.quantity; // Vérifier cette ligne
+                                // console.log(itemsLocalStorage[i].quantity);
+                                localStorage.setItem('item', JSON.stringify(itemsLocalStorage));
+                                // console.log(itemsLocalStorage.length);
+                            }
+                        }    
+                        if(itemOk === 0){
+                                itemsLocalStorage.push(item);
+                                // console.log("Hallo"); // Vérifier
+                                localStorage.setItem('item', JSON.stringify(itemsLocalStorage));
+                                // console.log(itemsLocalStorage.length);
+                        }               
+                    }else{
+                        itemsLocalStorage = [];
+                        itemsLocalStorage.push(item);
+                        // console.log("Laurent");
+                        localStorage.setItem('item', JSON.stringify(itemsLocalStorage));
+                        // console.log(itemsLocalStorage.length);
+                    }
+                    }
+                })
         }
     })
     
